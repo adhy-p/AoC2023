52c359aa86cbb21e4a1a1205b6424f5de6ace558
adhy-p <adhyp02@gmail.com>
2023-12-12 19:55:28 +0800

    day 12: use slice instead of vector + index

--- day12/main.rs
+++ day12/main.rs
@@ -15,9 +15,11 @@ fn main() {
             .collect();
         let max_group_size: usize = *arragements1.iter().max().unwrap();
         let mut cache: Vec<Vec<Vec<usize>>> =
-            vec![vec![vec![usize::MAX; max_group_size + 1]; arragements1.len()]; map1.len()];
-
-        total_ways_1 += find_ways(&mut cache, &map1, &arragements1, 0, 0, 0);
+            vec![
+                vec![vec![usize::MAX; max_group_size + 1]; arragements1.len()];
+                map1.len()
+            ];
+        total_ways_1 += find_ways(&mut cache, &map1, &arragements1, 0);
 
         let mut map2 = map1.clone();
         let mut arragements2 = arragements1.clone();
@@ -27,8 +29,11 @@ fn main() {
             arragements2.extend(arragements1.clone());
         }
         let mut cache: Vec<Vec<Vec<usize>>> =
-            vec![vec![vec![usize::MAX; max_group_size + 1]; arragements2.len()]; map2.len()];
-        total_ways_2 += find_ways(&mut cache, &map2, &arragements2, 0, 0, 0);
+            vec![
+                vec![vec![usize::MAX; max_group_size + 1]; arragements2.len()];
+                map2.len()
+            ];
+        total_ways_2 += find_ways(&mut cache, &map2, &arragements2, 0);
     }
     println!("{}", total_ways_1);
     println!("{}", total_ways_2);
@@ -37,78 +42,63 @@ fn main() {
 
 fn find_ways(
     cache: &mut Vec<Vec<Vec<usize>>>,
-    map: &Vec<char>,
-    arragements: &Vec<usize>,
-    map_idx: usize,
-    arr_idx: usize,
+    map: &[char],
+    arragements: &[usize],
     curr_grp_size: usize,
 ) -> usize {
     // println!("find ways: {:?} {:?} {} {} {}", map, arragements, map_idx, arr_idx, curr_grp_size);
-    if arr_idx == arragements.len() {
-        if map_idx == map.len() || map[map_idx..].iter().filter(|x| **x == '#').count() == 0 {
+    if arragements.is_empty() {
+        if map.is_empty() || map.iter().filter(|x| **x == '#').count() == 0 {
             // println!("find ways: {:?} {:?} {} {} {} returns 1", map, arragements, map_idx, arr_idx, curr_grp_size);
             return 1;
         }
         return 0;
     }
-    if map_idx == map.len() {
-        if arr_idx == arragements.len() - 1 && curr_grp_size == arragements[arr_idx] {
+    if map.is_empty() {
+        if arragements.len() == 1 && curr_grp_size == arragements[0] {
             // println!("find ways: {:?} {:?} {} {} {} returns 1", map, arragements, map_idx, arr_idx, curr_grp_size);
             return 1;
         }
         return 0;
-    }
-    if cache[map_idx][arr_idx][curr_grp_size] != usize::MAX {
-        return cache[map_idx][arr_idx][curr_grp_size];
     }
 
-    cache[map_idx][arr_idx][curr_grp_size] = match map[map_idx] {
+    if cache[map.len()-1][arragements.len()-1][curr_grp_size] != usize::MAX {
+        return cache[map.len()-1][arragements.len()-1][curr_grp_size];
+    }
+
+    cache[map.len()-1][arragements.len()-1][curr_grp_size] = match map[0] {
         '#' => {
-            if curr_grp_size + 1 > arragements[arr_idx] {
+            if curr_grp_size + 1 > arragements[0] {
                 return 0;
             }
-            find_ways(
-                cache,
-                map,
-                arragements,
-                map_idx + 1,
-                arr_idx,
-                curr_grp_size + 1,
-            )
+            find_ways(cache, &map[1..], arragements, curr_grp_size + 1)
         }
         '.' => {
             if curr_grp_size != 0 {
-                if curr_grp_size != arragements[arr_idx] {
+                if curr_grp_size != arragements[0] {
                     return 0;
                 }
-                return find_ways(cache, map, arragements, map_idx + 1, arr_idx + 1, 0);
+                return find_ways(cache, &map[1..], &arragements[1..], 0);
             }
-            find_ways(cache, map, arragements, map_idx + 1, arr_idx, curr_grp_size)
+            find_ways(cache, &map[1..], arragements, curr_grp_size)
         }
         '?' => {
             let mut ways = 0;
             // case 1: #
-            if curr_grp_size + 1 <= arragements[arr_idx] {
-                ways += find_ways(
-                    cache,
-                    map,
-                    arragements,
-                    map_idx + 1,
-                    arr_idx,
-                    curr_grp_size + 1,
-                );
+            if curr_grp_size < arragements[0] {
+                ways += find_ways(cache, &map[1..], arragements, curr_grp_size + 1);
             }
             // case 2: .
             if curr_grp_size != 0 {
-                if curr_grp_size == arragements[arr_idx] {
-                    ways += find_ways(cache, map, arragements, map_idx + 1, arr_idx + 1, 0);
+                if curr_grp_size == arragements[0] {
+                    ways += find_ways(cache, &map[1..], &arragements[1..], 0);
                 }
             } else {
-                ways += find_ways(cache, map, arragements, map_idx + 1, arr_idx, curr_grp_size)
+                ways += find_ways(cache, &map[1..], arragements, curr_grp_size)
             }
             ways
         }
         _ => panic!("invalid map input"),
     };
-    cache[map_idx][arr_idx][curr_grp_size]
+    cache[map.len()-1][arragements.len()-1][curr_grp_size]
 }
