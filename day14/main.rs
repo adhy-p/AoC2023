cda558b61d5c64bd02f2b871fe3e0bc73bb73996
adhy-p <adhyp02@gmail.com>
2023-12-15 02:15:58 +0800

    day 14: refactor

--- day14/main.rs
+++ day14/main.rs
@@ -1,7 +1,6 @@
 use core::fmt;
 use std::collections::HashMap;
 use std::fs::read_to_string;
-use std::mem::swap;
 use std::time::Instant;
 
 fn calc_load(map: &[Vec<char>]) -> usize {
@@ -12,37 +11,28 @@ fn calc_load(map: &[Vec<char>]) -> usize {
         .sum()
 }
 
-enum Direction {
-    Up,
-    Left,
-    Down,
-    Right,
-}
-
 fn shift_left(map: &mut Vec<Vec<char>>) {
+    let n_rows = map.len();
     let n_cols = map[0].len();
-    for row in map {
-        let mut dot_idx = n_cols;
-        for i in 0..n_cols {
-            match row[i] {
-                'O' => {
-                    if dot_idx != n_cols {
-                        row.swap(dot_idx, i);
-                        while row[dot_idx] != '.' {
-                            dot_idx += 1;
-                            if dot_idx == n_cols {
-                                break;
-                            }
+
+    for row in 0..n_rows {
+        let mut dot_idx = 0;
+        for col in 0..n_cols {
+            match map[row][col] {
+                'O' if dot_idx != n_cols => {
+                    while dot_idx < col && map[row][dot_idx] != '.' {
+                        dot_idx += 1;
+                        if dot_idx == n_cols {
+                            break;
                         }
                     }
-                }
-                '.' => {
-                    if dot_idx == n_cols {
-                        dot_idx = i;
+                    if dot_idx != col {
+                        map[row][dot_idx] = 'O';
+                        map[row][col] = '.';
                     }
                 }
                 '#' => {
-                    dot_idx = n_cols;
+                    dot_idx = col;
                 }
                 _ => {}
             }
@@ -51,30 +41,24 @@ fn shift_left(map: &mut Vec<Vec<char>>) {
 }
 
 fn shift_right(map: &mut Vec<Vec<char>>) {
-    let n_colss = map[0].len();
-    for row in map {
-        let mut dot_idx = n_colss;
-        for i in (0..n_colss).rev() {
-            match row[i] {
+    let n_rows = map.len();
+    let n_cols = map[0].len();
+
+    for row in 0..n_rows {
+        let mut dot_idx = n_cols - 1;
+        for col in (0..n_cols).rev() {
+            match map[row][col] {
                 'O' => {
-                    if dot_idx != row.len() {
-                        row.swap(dot_idx, i);
-                        while row[dot_idx] != '.' {
-                            if dot_idx == 0 {
-                                dot_idx = n_colss;
-                                break;
-                            }
-                            dot_idx -= 1;
-                        }
+                    while dot_idx > col && map[row][dot_idx] != '.' {
+                        dot_idx -= 1;
                     }
-                }
-                '.' => {
-                    if dot_idx == n_colss {
-                        dot_idx = i;
+                    if dot_idx != col {
+                        map[row][col] = '.';
+                        map[row][dot_idx] = 'O';
                     }
                 }
                 '#' => {
-                    dot_idx = n_colss;
+                    dot_idx = col;
                 }
                 _ => {}
             }
@@ -87,28 +71,20 @@ fn shift_up(map: &mut Vec<Vec<char>>) {
     let n_cols = map[0].len();
 
     for col in 0..n_cols {
-        let mut dot_idx = n_rows;
+        let mut dot_idx = 0;
         for row in 0..n_rows {
             match map[row][col] {
-                'O' => {
-                    if dot_idx != n_rows {
-                        let (dot, rock) = map.split_at_mut(row);
-                        swap(&mut rock[0][col], &mut dot[dot_idx][col]);
-                        while map[dot_idx][col] != '.' {
-                            dot_idx += 1;
-                            if dot_idx == n_rows {
-                                break;
-                            }
-                        }
+                'O' if dot_idx != n_rows => {
+                    while dot_idx < row && map[dot_idx][col] != '.' {
+                        dot_idx += 1;
                     }
-                }
-                '.' => {
-                    if dot_idx == n_rows {
-                        dot_idx = row;
+                    if dot_idx != row {
+                        map[dot_idx][col] = 'O';
+                        map[row][col] = '.';
                     }
                 }
                 '#' => {
-                    dot_idx = n_rows;
+                    dot_idx = row;
                 }
                 _ => {}
             }
@@ -121,45 +97,24 @@ fn shift_down(map: &mut Vec<Vec<char>>) {
     let n_cols = map[0].len();
 
     for col in 0..n_cols {
-        let mut dot_idx = n_rows;
+        let mut dot_idx = n_rows - 1;
         for row in (0..n_rows).rev() {
             match map[row][col] {
                 'O' => {
-                    if dot_idx != n_rows {
-                        // split at mut is honestly annoying when rev is involved
+                    while dot_idx > row && map[dot_idx][col] != '.' {
+                        dot_idx -= 1;
+                    }
+                    if dot_idx != row {
                         map[dot_idx][col] = 'O';
                         map[row][col] = '.';
-                        while map[dot_idx][col] != '.' {
-                            dot_idx -= 1;
-                            if dot_idx == 0 {
-                                dot_idx = n_rows;
-                                break;
-                            }
-                        }
-                    }
-                }
-                '.' => {
-                    if dot_idx == n_rows {
-                        dot_idx = row;
                     }
                 }
                 '#' => {
-                    dot_idx = n_rows;
+                    dot_idx = row;
                 }
                 _ => {}
             }
         }
-    }
-}
-
-fn tilt(map: &mut Vec<Vec<char>>, dir: Direction) {
-    match dir {
-        Direction::Up => shift_up(map),
-        Direction::Left => shift_left(map),
-        Direction::Down => {
-            shift_down(map);
-        }
-        Direction::Right => shift_right(map),
     }
 }
 
@@ -177,26 +132,27 @@ impl fmt::Display for VecPrinter {
         Ok(())
     }
 }
+
 /* part 2 */
 fn main() {
     let start = Instant::now();
+
     let input = read_to_string("./input").unwrap();
-    let mut total: usize = 0;
     let mut map: Vec<Vec<char>> = input.lines().map(|row| row.chars().collect()).collect();
+
+    let mut total: usize = 0;
     const NUM_ITER: usize = 1_000_000_000;
-    const DIRS: [Direction; 4] = [
-        Direction::Up,
-        Direction::Left,
-        Direction::Down,
-        Direction::Right,
-    ];
+
     let mut history: HashMap<Vec<Vec<char>>, usize> = HashMap::new();
     history.insert(map.clone(), 0);
-    let mut remain = usize::MAX;
+
+    let mut remain = 0;
     for i in 1..=NUM_ITER {
-        for d in DIRS {
-            tilt(&mut map, d);
-        }
+        shift_up(&mut map);
+        shift_left(&mut map);
+        shift_down(&mut map);
+        shift_right(&mut map);
+
         if !history.contains_key(&map) {
             history.insert(map.clone(), i);
         } else {
@@ -210,15 +166,15 @@ fn main() {
     }
 
     for _ in 0..remain {
-        for d in DIRS {
-            tilt(&mut map, d);
-        }
+        shift_up(&mut map);
+        shift_left(&mut map);
+        shift_down(&mut map);
+        shift_right(&mut map);
     }
     total += calc_load(&map);
     println!("{}", total);
     println!("{:?}", start.elapsed());
 }
-
 
 /* part 1 */
 // fn main() {
