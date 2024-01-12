1ee48639d0505a61371cdd19cb88a023979fe280
adhy-p <adhyp02@gmail.com>
2024-01-12 21:30:23 +0800

    day 25: Speedup
    Update implementation to get most tightly connected vertex.
    Improve runtime from 4 minutes to ~1 seconds.

--- day25/main.rs
+++ day25/main.rs
@@ -5,17 +5,17 @@ fn main() {
     let start = Instant::now();
 
     let input = include_str!("./input");
-    let mut graph = BTreeMap::new();
+    let mut graph: BTreeMap<&str, BTreeMap<&str, usize>> = BTreeMap::new();
     for line in input.lines() {
         let (src, dst) = line.split_once(": ").unwrap();
         let dst = dst.split(' ').collect::<Vec<_>>();
         for d in dst {
-            graph.entry(d).or_insert(BTreeMap::new()).insert(src, 1);
-            graph.entry(src).or_insert(BTreeMap::new()).insert(d, 1);
+            graph.entry(d).or_default().insert(src, 1);
+            graph.entry(src).or_default().insert(d, 1);
         }
     }
     // let mut graph = gen_test();
-    println!("{:?}", graph);
+    // println!("{:?}", graph);
 
     // randomly select a start node
     let start_node = *graph.keys().next().unwrap();
@@ -33,26 +33,28 @@ fn minimum_cut<'a>(
 ) -> usize {
     // println!("start_node: {}", start_node);
     let n = graph.len();
-    let mut min_cut = graph.len();
+    let mut min_cut_cost = graph.len();
+
     // a map that keep tracks nodes that has 'consumed' other nodes
     let mut consumed_nodes: BTreeMap<&str, BTreeSet<&str>> = BTreeMap::from_iter(
         graph
             .keys()
             .map(|k| (*k, BTreeSet::from_iter(std::iter::once(*k)))),
     );
+
     while graph.len() > 1 {
         let (cut_phase_cost, partition) = minimum_cut_phase(graph, &mut consumed_nodes, start_node);
-        if cut_phase_cost < min_cut {
-            min_cut = cut_phase_cost;
+        if cut_phase_cost < min_cut_cost {
+            min_cut_cost = cut_phase_cost;
         }
-        if min_cut == 3 {
+        if cut_phase_cost == 3 {
             let ans = partition.len() * (n - partition.len());
             println!("ans: {}", ans);
-            return min_cut;
+            // return min_cut_cost;
         }
-        println!("graph len: {}, cut_cost: {}", graph.len(), cut_phase_cost);
+        // println!("graph len: {}, cut_cost: {}", graph.len(), cut_phase_cost);
     }
-    min_cut
+    min_cut_cost
 }
 
 fn minimum_cut_phase<'a>(
@@ -61,17 +63,27 @@ fn minimum_cut_phase<'a>(
     start_node: &'a str,
 ) -> (usize, BTreeSet<&'a str>) {
     let mut a_set: BTreeSet<&str> = BTreeSet::new();
+    let mut a_set_neighbors: BTreeMap<&str, usize> = BTreeMap::new();
     let mut last_two_added = VecDeque::new();
     a_set.insert(start_node);
-    last_two_added.push_back(start_node);
+    update_a_set_neighbors(graph, &mut a_set, &mut a_set_neighbors, start_node);
+    // println!("{:?}", a_set);
+    // println!("{:?}", a_set_neighbors);
 
+    if graph.len() == 2 {
+        last_two_added.push_back(start_node);
+    }
     while a_set.len() != graph.len() {
-        let v = get_most_tightly_connected_vertex(graph, &a_set);
-        // println!("added to a_set: {}", v);
-        a_set.insert(v);
-        last_two_added.push_back(v);
-        if last_two_added.len() > 2 {
-            last_two_added.pop_front();
+        // get most tightly connected vertex
+        let most_tightly_con_v = get_most_tightly_connected_vertex(&a_set_neighbors);
+        update_a_set_neighbors(graph, &mut a_set, &mut a_set_neighbors, most_tightly_con_v);
+        // println!("added to a_set: {}", most_tightly_con_v);
+        // println!("{:?}", a_set);
+        // println!("{:?}", a_set_neighbors);
+        // println!("a_set len: {}, graph len: {}", a_set.len(), graph.len());
+
+        if graph.len() - a_set.len() < 2 {
+            last_two_added.push_back(most_tightly_con_v);
         }
     }
     assert_eq!(last_two_added.len(), 2);
@@ -82,39 +94,45 @@ fn minimum_cut_phase<'a>(
     // cut of the phase: the cut of V that separates the vertex added last
     // from the rest of the graph
     let cut_cost = graph.get(last).unwrap().values().sum::<usize>();
-    println!("cut_cost: {}", cut_cost);
+
+    // println!("cut_cost: {}", cut_cost);
     // println!("graph before merging: {:?}", graph);
+
     // we want the group just before the nodes are merged
     let partition = consumed_nodes.get(last).unwrap().clone();
     merge_vertices(graph, consumed_nodes, second_last, last);
     // println!("graph after merging: {:?}", graph);
+
     (cut_cost, partition)
 }
 
-fn get_most_tightly_connected_vertex<'a>(
-    graph: &mut BTreeMap<&'a str, BTreeMap<&'a str, usize>>,
-    a_set: &BTreeSet<&'a str>,
-) -> &'a str {
-    let mut max_weight = 0;
-    let mut max_vertex = "";
-    for (&node, neighbors) in graph.iter() {
-        if a_set.contains(&node) {
-            continue;
-        }
-        let mut w = 0;
-        for (&n, &weight) in neighbors {
-            if a_set.contains(&n) {
-                w += weight;
-            }
-            if w > max_weight {
-                max_weight = w;
-                max_vertex = node;
-            }
-        }
-    }
-    max_vertex
+fn get_most_tightly_connected_vertex<'a>(a_set_neighbors: &BTreeMap<&'a str, usize>) -> &'a str {
+    a_set_neighbors
+        .iter()
+        .max_by(|lhs, rhs| lhs.1.cmp(rhs.1))
+        .map(|(k, _v)| k)
+        .unwrap()
 }
 
+fn update_a_set_neighbors<'a>(
+    graph: &BTreeMap<&'a str, BTreeMap<&'a str, usize>>,
+    a_set: &mut BTreeSet<&'a str>,
+    a_set_neighbors: &mut BTreeMap<&'a str, usize>,
+    to_remove: &'a str,
+) {
+    a_set.insert(to_remove);
+    let neighbors = graph.get(to_remove).unwrap();
+    for (n, weight) in neighbors {
+        if a_set.contains(n) {
+            continue;
+        }
+        a_set_neighbors
+            .entry(n)
+            .and_modify(|w| *w += *weight)
+            .or_insert(*weight);
+    }
+    a_set_neighbors.remove(to_remove);
+}
 fn merge_vertices<'a>(
     graph: &mut BTreeMap<&'a str, BTreeMap<&'a str, usize>>,
     consumed_nodes: &mut BTreeMap<&'a str, BTreeSet<&'a str>>,
@@ -150,11 +168,14 @@ fn merge_vertices<'a>(
     consumer_map.remove(consumed);
 
     consumed_nodes.entry(consumer).or_default().insert(consumed);
-    if let Some(mut consumed) = consumed_nodes.remove(consumed) {
-        consumed_nodes
-            .entry(consumer)
-            .or_default()
-            .append(&mut consumed);
+    if let Some(consumed) = consumed_nodes.remove(consumed) {
+        // consumed_nodes
+        //     .entry(consumer)
+        //     .or_default()
+        //     .append(&mut consumed);
+        for c in consumed {
+            consumed_nodes.entry(consumer).or_default().insert(c);
+        }
     }
 }
 
